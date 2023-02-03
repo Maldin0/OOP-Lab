b84eba3984a3b68d885ad011492caaa4d0797e59
Madino <65070015@kmitl.ac.th>
2023-02-03 09:37:27 +0700

    Fix OddEven.java

--- src/Lab3/OddEven.java
+++ src/Lab3/OddEven.java
@@ -1,5 +1,7 @@
 package Lab3;
+
 import java.util.Scanner;
+
 /**
  * @author Maldin0
  * @created 2/3/2023 - 12:22 AM
@@ -9,9 +11,9 @@ public class OddEven {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int odd = 0, even = 0, num;
-        while (true){
+        while (true) {
             num = input.nextInt();
-            if (num != -1){
+            if (num != -1) {
                 if (num % 2 == 0) {
                     even++;
                 } else {
@@ -21,6 +23,6 @@ public class OddEven {
                 break;
             }
         }
-        System.out.println("Odd number = "+odd+" and Even number = "+even);
+        System.out.println("Odd number = " + odd + " and Even number = " + even);
     }
 }
